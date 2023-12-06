a1c6275c879dc18fc422a95a23fe604852a7ac49
Leopold von wendt <91526397+lvonw@users.noreply.github.com>
2023-12-06 12:56:28 +0100

    c und d
    d gibt die rückgabe noch nicht alles aus, irgendwie gibt es da ein return limit

---
+++ startup.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+BLUE='\033[0;34m'
+NC='\033[0m'
+
+cd ./deploy
+echo -e "${BLUE}Starting up the Docker-Compose stack...${NC}"
+docker compose up -d
--- transferService/src/transferService.py
+++ transferService/src/transferService.py
@@ -1,8 +1,12 @@
-import os
-import boto3
-import psycopg
-import constants
+import  os
+import  boto3
+import  psycopg
+import  constants
 
+from    tqdm        import tqdm
+
+def log (msg):
+    print(GREEN + msg + CLEAR)
 
 def test(expected, result, desc):
     passed = expected == result
@@ -25,10 +29,6 @@ GREEN   = "\033[92m"
 BLUE    = "\033[94m"
 CLEAR   = "\033[0m"
 
-# List all tables in DynamoDB
-table_names = client.list_tables()["TableNames"]
-print(table_names)
-
 # ======================================================================
 # As we decided to follow the best practice, and recommendation of the 
 # DynamoDB documentation we utilize the single table design scheme.
@@ -46,8 +46,12 @@ with psycopg.connect(dbname     = constants.POSTGRES_DB_NAME,
 
     # Open a cursor to perform database operations
     with conn.cursor() as cur:
-
+        print("========== CREATE ============================================")
         # ========== ACCESS PATTERN 1 ==================================
+        # As we use a single table approach, it would be inefficient to
+        # scan through each partition key, to see which rows are actual
+        # inventory items. Therefore we employ a master partition which
+        # allows us to query all movies with a single partition key.
 
         # cur.execute("SELECT inventory_id FROM inventory")
         # results = cur.fetchall()
@@ -69,22 +73,84 @@ with psycopg.connect(dbname     = constants.POSTGRES_DB_NAME,
         #             constants.TABLE_SORTING_KEY:    "FLM#" + str(record[1])})
 
         # ========== ACCESS PATTERN 3 ==================================
+        # We again use a master partition to be able to query all actors
+        # without looking through all partition keys, and then use a 
+        # composite sorting key to associate an actor with a movie they
+        # made. As we havent really found a good way to sort based on 
+        # amount of occurence in Dynamo, we will have to do this later 
+        # in the client code.  
+
+        # log("Querying data from postgres for access pattern 3")
+        # cur.execute("""
+        #             SELECT fa.actor_id, film_id, first_name, last_name 
+        #             FROM film_actor AS fa
+        #             INNER JOIN actor AS a
+        #             ON fa.actor_id = a.actor_id""")
+        # results = cur.fetchall()
+
+        # log("Inserting into DynamoDB")
+        # for record in results:
+        #     table.put_item(
+        #         Item={
+        #             constants.TABLE_PARTITION_KEY:  "MACT#1",
+        #             constants.TABLE_SORTING_KEY:    "ACT#" + str(record[0]) +
+        #                                             "FLM#" + str(record[1]),
+        #             "first_name": str(record[2]),
+        #             "last_name": str(record[3])})
+            
+        # log("Done\n")
+
+        # ========== ACCESS PATTERN 4 ==================================
+        # Associate staff with payments via secondary index. We do it in
+        # this fashion, because this allows us to do the same with the 
+        # customers in Access Pattern 7. 
+        log("Querying data from postgres for access pattern 4")
         cur.execute("""
-                    SELECT fa.actor_id, film_id, first_name, last_name 
-                    FROM film_actor AS fa
-                    INNER JOIN actor AS a
-                    ON fa.actor_id = a.actor_id""")
+                    SELECT payment_id, staff_id, amount
+                    FROM payment """)
         results = cur.fetchall()
 
-        # TODO Höhere Ebene, damit mann die anzahl der schauspieler sehen kann
-        for record in results:
-            table.put_item(
-                Item={
-                    constants.TABLE_PARTITION_KEY:  "ACT#" + str(record[0]),
-                    constants.TABLE_SORTING_KEY:    "FLM#" + str(record[1]),
-                    "first_name": str(record[2]),
-                    "last_name": str(record[3])})
+        log("Inserting into DynamoDB")
+        # table.update(
+        #     AttributeDefinitions=[
+        #         {"AttributeName": "GSI-1-PK", "AttributeType": "S"},
+        #         {"AttributeName": "GSI-1-SK", "AttributeType": "S"},
+        #         {"AttributeName": "amount",   "AttributeType": "N"}],
+        #     GlobalSecondaryIndexUpdates=[{ 
+        #         "Create": {
+        #             "IndexName" : "GSI-1",
+        #             "KeySchema" : [
+        #                 {"AttributeName" : "GSI-1-PK", "KeyType" : "HASH"},
+        #                 {"AttributeName" : "GSI-1-SK", "KeyTyps" : "RANGE"}],
+        #             "Projection" : {
+        #                 "ProjectionType" : "ALL"
+        #             }
+        #         }
+        #     }]
+        # )
+        
+        # put_items = []
+        # for record in results:
+        #     item = {
+        #         constants.TABLE_PARTITION_KEY:  "MPAY#1",
+        #         constants.TABLE_SORTING_KEY:    "PAY#" + str(record[0]),
+        #         "GSI-1-PK"  : "MSTF#1",
+        #         "GSI-1-SK"  : "STF#" + str(record[1]),
+        #         "amount"    : record[2]}
+        #     put_items.append({"PutRequest": {"Item": item}})
+        
+        # # Batch write items
+        # with table.batch_writer() as batch:
+        #     for item in put_items:
+        #         batch.put_item(Item=item["PutRequest"]["Item"])
 
+        # log("Done\n")
+        # ========== ACCESS PATTERN 5 ==================================
+        # ========== ACCESS PATTERN 6 ==================================
+        # ========== ACCESS PATTERN 7 ==================================
+        # ========== ACCESS PATTERN 8 ==================================
+        # ========== ACCESS PATTERN 9 ==================================
+        print("========== READ ==============================================")
 
         # ========== 4.A ===============================================
         response = table.query(
@@ -112,6 +178,44 @@ with psycopg.connect(dbname     = constants.POSTGRES_DB_NAME,
         b4 = (b41, b42)
 
         # ========== 4.C ===============================================
+        # ========== 4.D ===============================================
+        response = table.query(
+            IndexName="GSI-1",
+            KeyConditionExpression=
+                boto3.dynamodb.conditions.Key("GSI-1-PK").eq("MSTF#1") &
+                boto3.dynamodb.conditions.Key("GSI-1-SK").eq("STF#2")
+        )
+        print(response["Count"])
+
+        response = table.query(
+            IndexName="GSI-1",
+            KeyConditionExpression=
+                boto3.dynamodb.conditions.Key("GSI-1-PK").eq("MSTF#1") &
+                boto3.dynamodb.conditions.Key("GSI-1-SK").eq("STF#1")
+        )
+        print(response["Count"])
+
+        response = table.query(
+            IndexName="GSI-1",
+            KeyConditionExpression=
+                boto3.dynamodb.conditions.Key("GSI-1-PK").eq("MSTF#1")
+        )
+
+        print(response["ConsumedCapacity"])
+
+        d4 = {}
+        for item in response["Items"]:
+            if item["GSI-1-SK"] in d4: 
+                d4[item["GSI-1-SK"]] += item["amount"]   
+            else:
+                d4[item["GSI-1-SK"]] = item["amount"] 
+        print(d4)
+        # ========== 4.E ===============================================
+        # ========== 4.F ===============================================
+        # ========== 4.G ===============================================
+        # ========== 4.H ===============================================
+        # ========== 4.I ===============================================
+
 
 
 
